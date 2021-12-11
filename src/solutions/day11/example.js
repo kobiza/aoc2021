1918d241246f19714a7f87ea71d2ffc229322e2c
kobiz <kobiz@wix.com>
2021-12-11 08:08:58 +0200

    add day11

---
+++ src/solutions/day11/example.js
@@ -0,0 +1,7 @@
+const example = `11111
+19991
+19191
+19991
+11111`
+
+export default example
--- src/solutions/solutionsMap.js
+++ src/solutions/solutionsMap.js
@@ -1,9 +1,11 @@
 import day08 from './day08/index'
 import day09 from './day09/index'
 import day10 from './day10/index'
+import day11 from './day11/index'
 
 export default {
 	day08,
 	day09,
 	day10,
+	day11,
 }
