8cf9795ffa26a14a616c2b43e2b5ebf9db52e4c1
kobiz <kobiz@wix.com>
2021-12-12 15:29:02 +0200

    add day 12

--- src/solutions/solutionsMap.js
+++ src/solutions/solutionsMap.js
@@ -2,10 +2,12 @@ import day08 from './day08/index'
 import day09 from './day09/index'
 import day10 from './day10/index'
 import day11 from './day11/index'
+import day12 from './day12/index'
 
 export default {
 	day08,
 	day09,
 	day10,
 	day11,
+	day12,
 }
