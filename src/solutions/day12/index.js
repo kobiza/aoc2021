10ae35d2b6e65abc8b3dde1832942d8a0a41d7b5
kobiz <kobiz@wix.com>
2021-12-13 21:52:00 +0200

    day 12 partial solution

--- src/solutions/day12/index.js
+++ src/solutions/day12/index.js
@@ -4,14 +4,86 @@ import example from './example'
 import {part1Tests, part2Tests} from './testsData'
 import testRunner from "../../utils/testRunner";
 
-const inputParser = (input) => input.split('\n')
+const toTree = (arr) => {
+    let result = {}
+    _.forEach(arr, ([src, dest]) => {
+        result[src] = result[src] || []
+        result[dest] = result[dest] || []
 
-const part1 = (input) => {
-    return _.identity(input)
+        result[src].push(dest)
+        result[dest].push(src)
+    })
+
+    return result
 }
 
-const part2 = (input) => {
-    return _.identity(input)
+const inputParser = (input) => toTree(input.split('\n').map(v => v.split('-')))
+
+const part1 = (tree) => {
+    const isSmallCave = str => _.upperCase(str) !== str
+
+    const recPathFinder = (cell, visited = {}) => {
+        if (cell === 'end') {
+            return [[cell]]
+        }
+
+        const newVisited = {
+            ...visited,
+        }
+
+        const directions = tree[cell].filter(v => !visited[v] && v !== 'start')
+
+        if (isSmallCave(cell)) {
+            newVisited[cell] = true
+        }
+
+        const toAdd = [].concat(_.flatten(directions.map((v) => recPathFinder(v, newVisited))))
+
+        return toAdd.map(arr => [cell, ...arr])
+    }
+
+    return recPathFinder('start')
+}
+
+const part2 = (tree) => {
+    const isSmallCave = str => _.upperCase(str) !== str
+
+    const recPathFinder = (cell, visited = {}, alreadySkippedVisit = false) => {
+        if (cell === 'end') {
+            return [[cell]]
+        }
+
+        // const newVisited = {
+        //     ...visited,
+        // }
+
+        const possibleDirections = tree[cell].filter(v => !visited[v] && v !== 'start')
+        let directions
+
+        if (cell === 'start') {
+            directions = possibleDirections.map(v => ({cellKey: v, visited: {...visited, [cell]: true}, alreadySkippedVisit}))
+        } else if (isSmallCave(cell)) {
+            // newVisited[cell] = true
+            const morePathsToCheck = alreadySkippedVisit ? [] : possibleDirections.map(v => ({cellKey: v, visited: {...visited}, alreadySkippedVisit: true}))
+
+            // todo -fix the code here
+            // here we getting duplicate paths from the two directions, currently i used unique on the result
+            directions = [
+                ...possibleDirections.map(v => ({cellKey: v, visited: {...visited, [cell]: true}, alreadySkippedVisit})),
+                ...morePathsToCheck
+            ]
+        } else {
+            directions = possibleDirections.map(v => ({cellKey: v, visited: {...visited}, alreadySkippedVisit}))
+        }
+
+        const toAdd = [].concat(_.flatten(directions.map((params) => {
+            return recPathFinder(params.cellKey, params.visited, params.alreadySkippedVisit)
+        }).filter(v => v)))
+
+        return toAdd.map(arr => [cell, ...arr])
+    }
+
+    return recPathFinder('start')
 }
 
 const runPart1Tests = () => testRunner.runTests(part1, part1Tests)
--- src/solutions/day12/input.js
+++ src/solutions/day12/input.js
@@ -1,3 +1,25 @@
-const input = ``
+const input = `he-JK
+wy-KY
+pc-XC
+vt-wy
+LJ-vt
+wy-end
+wy-JK
+end-LJ
+start-he
+JK-end
+pc-wy
+LJ-pc
+at-pc
+xf-XC
+XC-he
+pc-JK
+vt-XC
+at-he
+pc-he
+start-at
+start-XC
+at-LJ
+vt-JK`
 
 export default input
